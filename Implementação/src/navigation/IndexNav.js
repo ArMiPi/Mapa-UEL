84ed651ff96569ef7c7c52986beeb39b881f7be3
Valdi <llucasvaldi@gmail.com>
2023-12-02 20:24:06 -0300

    Teste de commit

--- Implementação/src/navigation/IndexNav.js
+++ Implementação/src/navigation/IndexNav.js
@@ -14,6 +14,7 @@ export default function IndexNav(){
   return (
     <Tab.Navigator
       screenOptions={{headerShown: false, tabBarActiveTintColor: "#42C83C", tabBarInactiveTintColor: "#737373", tabBarStyle: {backgroundColor: "#343434", height: 75, paddingTop: 10},
+      tabBarLabelPosition: "below-icon",
       tabBarLabelStyle: {fontSize: 15, fontWeight: "500", marginBottom: 10}}}
       initialRouteName='Eventos' >
       <Tab.Screen name="Eventos" component={EventsNav} options={{tabBarIcon: ({color}) => (<FontAwesome name="reorder" size={24} color= {color}/>)}}/>
